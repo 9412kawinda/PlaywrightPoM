5f910c9e206301fc2460ca641777a5b64cc69515
Kawinda Wimalarathna <kawindawimalarathna@gmail.com>
2023-08-10 10:37:35 +0530

    Expected scenario after logged in to the page

--- TestAppPOM/tests/pageObjectTest/TC001.test.js
+++ TestAppPOM/tests/pageObjectTest/TC001.test.js
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { LoginPage } from '../../page/Login.page';
 
 test.describe("TC001",()=>{
@@ -6,5 +6,6 @@ test.describe("TC001",()=>{
         const login = new LoginPage(page);
         await login.gotoLoginPage();
         await login.login('standard_user','secret_sauce');
+        await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
     });
 })
