0c0e9cf5a278c725c560a5f99b28ce83ee3ec752
Kawinda Wimalarathna <kawindawimalarathna@gmail.com>
2023-08-10 21:19:21 +0530

    Negative scenarios for passwprd

--- TestAppPOM/tests/pageObjectTest/TC001.test.js
+++ TestAppPOM/tests/pageObjectTest/TC001.test.js
@@ -10,17 +10,31 @@ test.describe("TC001", () => {
         await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
     });
 
-    test("Login negative_01",async({page})=>{
+    test("Login negative_01", async ({ page }) => {
         const login = new LoginPage(page);
         await login.gotoLoginPage();
         await login.login('abc', 'secret_sauce');
         await expect(page.getByText("Epic sadface: Username and password do not match any user in this service")).toBeVisible();
     });
 
-    test("Login negative_02",async({page})=>{
+    test("Login negative_02", async ({ page }) => {
         const login = new LoginPage(page);
         await login.gotoLoginPage();
         await login.login('', 'secret_sauce');
         await expect(page.getByText("Epic sadface: Username is required")).toBeVisible();
+    });
+
+    test("Login negative_03",async({page})=>{
+        const login = new LoginPage(page);
+        await login.gotoLoginPage();
+        await login.login('standard_user', 'abc');
+        await expect(page.getByText("Epic sadface: Username and password do not match any user in this service")).toBeVisible();
+    });
+
+    test("Login negative_04",async({page})=>{
+        const login = new LoginPage(page);
+        await login.gotoLoginPage();
+        await login.login('standard_user', '');
+        await expect(page.getByText("Epic sadface: Password is required")).toBeVisible();
     });
 })
