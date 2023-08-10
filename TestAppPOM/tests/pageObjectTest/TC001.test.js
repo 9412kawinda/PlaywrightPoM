df9557e82a49731f53f53151047a3e82647e643e
Kawinda Wimalarathna <kawindawimalarathna@gmail.com>
2023-08-10 21:07:27 +0530

    Two negative scenarios for login funtion

--- TestAppPOM/tests/pageObjectTest/TC001.test.js
+++ TestAppPOM/tests/pageObjectTest/TC001.test.js
@@ -1,11 +1,26 @@
 import { expect, test } from '@playwright/test';
 import { LoginPage } from '../../page/Login.page';
 
-test.describe("TC001",()=>{
-    test("Login positive", async ({page}) => {
+test.describe("TC001", () => {
+
+    test("Login positive", async ({ page }) => {
         const login = new LoginPage(page);
         await login.gotoLoginPage();
-        await login.login('standard_user','secret_sauce');
+        await login.login('standard_user', 'secret_sauce');
         await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+    });
+
+    test("Login negative_01",async({page})=>{
+        const login = new LoginPage(page);
+        await login.gotoLoginPage();
+        await login.login('abc', 'secret_sauce');
+        await expect(page.getByText("Epic sadface: Username and password do not match any user in this service")).toBeVisible();
+    });
+
+    test("Login negative_02",async({page})=>{
+        const login = new LoginPage(page);
+        await login.gotoLoginPage();
+        await login.login('', 'secret_sauce');
+        await expect(page.getByText("Epic sadface: Username is required")).toBeVisible();
     });
 })
